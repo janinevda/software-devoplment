d85c5b235365a3b0b55f007acb7fb41c643df21c
janinevda <janinevdakker@hotmail.com>
2020-11-19 13:05:48 +0100

    Update socketServer - kopie.py
    inclusief west

--- unityTest/Assets/websocket/socketServer - kopie.py
+++ unityTest/Assets/websocket/socketServer - kopie.py
@@ -32,21 +32,30 @@ host = '127.0.0.1'
 port = 54000
 
 def receiveFromSimulator():
-    #car
+   #car
     Group01A_array = ["A1-1", "A1-2", "A3-1", "A3-2", "A3-3", "A3-4"]
     Group01B_array = ["A1-1", "A1-2", "A2-1", "A2-2", "A3-3", "A4-4"]
     Group01C_array = ["A1-1", "A1-2", "A1-3", "A2-1", "A2-2"]
     Group02A_array = ["A2-1", "A2-2", "A2-3", "A2-4", "A3-3", "A3-4"]
     Group02B_array = ["A2-1", "A2-2", "A1-1", "A1-2", "A3-3", "A3-4"]
+    GroupW1A_array = ["A4-1", "A4-2", "A4-3", "A4-4", "A5-3", "A5-4"]
+    GroupW2A_array = ["A4-3", "A4-4", "A5-3", "A5-4", "A6-1", "A6-2"]
+    GroupW2B_array = ["A4-3", "A4-4", "A6-1", "A6-2", "A6-3", "A6-4"]
+    GroupW3A_array = ["A5-1", "A5-2", "A5-3", "A5-4", "A6-1", "A6-2"]
 
     #fiets + voet
     GroupFV01_array = ["A2-3", "A2-4", "A3-3", "A3-4", "F1-1", "F1-2", "V1-1", "V1-2", "V1-3", "V1-4"]
     GroupFV02_array = ["A1-1", "A1-2", "A3-1", "A3-2", "F2-1", "F2-2", "V2-1", "V2-2", "V2-3", "V2-4"]
+    GroupFVW1_array = ["A5-1", "A5-2", "A6-1", "A6-2", "F4-1", "F4-2", "V4-1", "V4-2", "V4-3", "V4-4"]
+    GroupFVW2_array = ["A4-3", "A4-3", "A6-3", "A6-4", "F5-1", "F5-2", "V5-1", "V5-2", "V5-3", "V5-4"]
     
     #bus
     GroupB01A_array = ["A3-1", "A3-2", "A3-3", "A3-4", "B1-1"]
     GroupB01B_array = ["A2-1", "A2-2", "A3-3", "A3-4", "B1-1"]
     GroupB02_array = ["A1-1", "A1-2", "A2-1", "A2-2", "B1-2"]
+    GroupBW1A_array = ["A4-1", "A4-2", "A5-3", "A5-4", "B4-1"]
+    GroupBW1B_array = ["A5-3", "A5-4", "A6-1", "A6-2", "B4-1"]
+    GroupBW1C_array = ["A6-1", "A6-2", "A6-3", "A6-4", "B4-1"]
 
     
     TrafficLightsThatHasCarsEast = []
@@ -58,7 +67,9 @@ def receiveFromSimulator():
             TrafficLightsThatHasCarsEast.append(x)
             
     resultsEast = []
+    resultsWest = []
     amountOfTrafficlightMatchesEast = []
+    amountOfTrafficlightMatchesWest = []
     
     for i in range(10):
         resultsEast.append([])
@@ -93,6 +104,40 @@ def receiveFromSimulator():
             if(j == 1):
                 count += 1
         amountOfTrafficlightMatchesEast[i].append(count)
+        
+     for i in range(9):
+        resultsWest.append([])
+        amountOfTrafficlightMatchesWest.append([])
+
+    for i in TrafficLightsThatHasCarsEast:
+        resultsWest[0].append(GroupW1A_array.count(i))
+        resultsWest[1].append(GroupW2A_array.count(i))
+        resultsWest[2].append(GroupW2B_array.count(i))
+        resultsWest[3].append(GroupW3A_array.count(i))
+        resultsWest[4].append(GroupFVW1_array.count(i))
+        resultsWest[5].append(GroupFVW2_array.count(i))
+        resultsWest[6].append(GroupBW1A_array.count(i))
+        resultsWest[7].append(GroupBW1B_array.count(i))
+        resultsWest[8].append(GroupBW1C_array.count(i))
+
+    amountOfTrafficlightMatchesWest[0].append("groepW1A")
+    amountOfTrafficlightMatchesWest[1].append("groepW2A")
+    amountOfTrafficlightMatchesWest[2].append("groepW2B")
+    amountOfTrafficlightMatchesWest[3].append("groepW3A")
+    amountOfTrafficlightMatchesWest[4].append("groepFVW1")
+    amountOfTrafficlightMatchesWest[5].append("groepFVW2")
+    amountOfTrafficlightMatchesWest[6].append("groepBW1A")
+    amountOfTrafficlightMatchesWest[7].append("groepBW1B")
+    amountOfTrafficlightMatchesWest[8].append("groepBW1C")
+
+    for i in range(9):
+        count = 0
+        for j in resultsWest[i]:
+            if (j == 1):
+                count += 1
+
+        amountOfTrafficlightMatchesWest[i].append(count)
+        print(amountOfTrafficlightMatchesWest)
 
     highestAmountOfMatchesGroupNameEast = ""
     highestAmountOfMatchesNumberEast = 0
@@ -123,6 +168,36 @@ def receiveFromSimulator():
         GroupB01B()
     elif(highestAmountOfMatchesGroupNameEast == "groepB02"):
         GroupB02()
+    else:
+        print("nothing")
+        
+    highestAmountOfMatchesGroupNameWest = ""
+    highestAmountOfMatchesNumberWest = 0
+    for i in range(len(amountOfTrafficlightMatchesWest)):
+        if(amountOfTrafficlightMatchesWest[i][1] > highestAmountOfMatchesNumberWest):
+            highestAmountOfMatchesNumberWest = amountOfTrafficlightMatchesWest[i][1]
+            highestAmountOfMatchesGroupNameWest = amountOfTrafficlightMatchesWest[i][0]
+
+    print(highestAmountOfMatchesGroupNameWest)
+
+    if(highestAmountOfMatchesGroupNameWest == "groepW1A"):
+        GroupW1A()
+    elif(highestAmountOfMatchesGroupNameWest == "groepW2A"):
+        GroupW2A()
+    elif(highestAmountOfMatchesGroupNameWest == "groepW2B"):
+        GroupW2B()
+    elif(highestAmountOfMatchesGroupNameWest == "groepW3A"):
+        GroupW3A()
+    elif (highestAmountOfMatchesGroupNameWest == "groepFVW1"):
+        GroupFVW1()
+    elif (highestAmountOfMatchesGroupNameWest == "groepFVW2"):
+        GroupFVW2()
+    elif (highestAmountOfMatchesGroupNameWest == "groepBW1A"):
+        GroupBW1A()
+    elif (highestAmountOfMatchesGroupNameWest == "groepBW1B"):
+        GroupBW1B()
+    elif (highestAmountOfMatchesGroupNameWest == "groepBW1C"):
+        GroupBW1C()
     else:
         print("nothing")
 
@@ -209,6 +284,83 @@ def GroupB02():
     jason["A1-2"] = 1
     jason["A2-1"] = 1
     jason["B1-2"] = 1
+   
+def GroupW1A():
+    jason["A4-1"] = 1
+    jason["A4-2"] = 1
+    jason["A4-3"] = 1
+    jason["A4-4"] = 1
+    jason["A5-3"] = 1
+    jason["A5-4"] = 1
+
+def GroupW2A():
+    jason["A4-3"] = 1
+    jason["A4-4"] = 1
+    jason["A5-3"] = 1
+    jason["A5-4"] = 1
+    jason["A6-1"] = 1
+    jason["A6-2"] = 1
+
+def GroupW2B():
+    jason["A4-3"] = 1
+    jason["A4-4"] = 1
+    jason["A6-1"] = 1
+    jason["A6-2"] = 1
+    jason["A6-3"] = 1
+    jason["A6-4"] = 1
+
+def GroupW3A():
+    jason["A5-1"] = 1
+    jason["A5-2"] = 1
+    jason["A5-3"] = 1
+    jason["A5-4"] = 1
+    jason["A6-1"] = 1
+    jason["A6-2"] = 1
+
+def GroupFVW1():
+    jason["A5-1"] = 1
+    jason["A5-2"] = 1
+    jason["A6-1"] = 1
+    jason["A6-2"] = 1
+    jason["F4-1"] = 1
+    jason["F4-2"] = 1
+    jason["V4-1"] = 1
+    jason["V4-2"] = 1
+    jason["V4-3"] = 1
+    jason["V4-4"] = 1
+
+def GroupFVW2():
+    jason["A4-3"] = 1
+    jason["A4-4"] = 1
+    jason["A6-3"] = 1
+    jason["A6-4"] = 1
+    jason["F5-1"] = 1
+    jason["F5-2"] = 1
+    jason["V5-1"] = 1
+    jason["V5-2"] = 1
+    jason["V5-3"] = 1
+    jason["V5-4"] = 1
+
+def GroupBW1A():
+    jason["A4-1"] = 1
+    jason["A4-2"] = 1
+    jason["A5-3"] = 1
+    jason["A5-4"] = 1
+    jason["B4-1"] = 1
+
+def GroupBW1B():
+    jason["A5-3"] = 1
+    jason["A5-4"] = 1
+    jason["A6-1"] = 1
+    jason["A6-2"] = 1
+    jason["B4-1"] = 1
+
+def GroupBW1C():
+    jason["A6-1"] = 1
+    jason["A6-2"] = 1
+    jason["A6-3"] = 1
+    jason["A6-4"] = 1
+    jason["B4-1"] = 1
     
 def changeToSame(x):
     jason["A1-1"] = x
